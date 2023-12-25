1ef6fb708ce142e9eb69a904ce33f86fad651b98
karyanayandi <halo@yandi.me>
2023-12-25 20:28:38 +0700

    fix: response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import crypto from "crypto"
-import axios from "axios"
+import axios, { type AxiosError, type AxiosResponse } from "axios"
 
 export type ClosedPaymentCode =
   | "MYBVA"
@@ -41,7 +41,7 @@ export interface TripayConfigProps {
   isProduction?: boolean
 }
 
-export interface TripayReturnProps<T> {
+export interface TripayInputProps<T> {
   instruction: ({
     code,
     pay_code,
@@ -127,12 +127,203 @@ export interface OpenTransactionDetailProps {
   uuid: string
 }
 
+export interface InstructionReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    title: string
+    steps: string[]
+  }[]
+}
+
+export interface PaymentChannelReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    group: string
+    code: string
+    name: string
+    type: string
+    fee_merchant: {
+      flat: number
+      percent: number
+    }
+    total_fee: {
+      flat: number
+      percent: number
+    }
+    minimum_fee: number
+    maximum_fee: number
+    icon_url: string
+    active: boolean
+  }[]
+}
+
+export interface FeeCalculatorReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    code: string
+    name: string
+    fee: {
+      flat: number
+      percent: string
+      min: number | null
+      max: number | null
+    }
+    total_fee: {
+      merchant: number
+      customer: number
+    }
+  }[]
+}
+
+export interface TransactionsReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    reference: string
+    merchant_ref: string
+    payment_selection_type: string
+    payment_method: string
+    payment_name: string
+    customer_name: string
+    customer_email: string
+    customer_phone: string | null
+    callback_url: string | null
+    return_url: string | null
+    amount: number
+    fee_merchant: number
+    fee_customer: number
+    total_fee: number
+    amount_received: number
+    pay_code: number
+    pay_url: string | null
+    checkout_url: string
+    order_items: [
+      {
+        sku: string | null
+        name: string
+        price: number
+        quantity: number
+        subtotal: number
+      },
+    ]
+    status: string
+    note: string | null
+    created_at: number
+    expired_at: number
+    paid_at: number | null
+  }[]
+  pagination: {
+    sort: string
+    offset: {
+      from: number
+      to: number
+    }
+    current_page: number
+    previous_page: number | null
+    next_page: number | null
+    last_page: number
+    per_page: number
+    total_records: number
+  }
+}
+
+export interface OpenTransactionsReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    reference: string
+    merchant_ref: string
+    payment_method: string
+    payment_name: string
+    customer_name: string
+    amount: number
+    fee_merchant: number
+    fee_customer: number
+    total_fee: number
+    amount_received: number
+    checkout_url: string
+    status: string
+    paid_at: number
+  }[]
+  pagination: {
+    total: number
+    data_from: number
+    data_to: number
+    per_page: number
+    current_page: number
+    last_page: number
+    next_page: number | null
+  }
+}
+
+export interface CreateOpenTransactionReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    uuid: string
+    merchant_ref: string
+    customer_name: string
+    payment_name: string
+    payment_method: string
+    pay_code: string
+    qr_string: string | null
+    qr_url: string | null
+  }
+}
+
+export interface CreateClosedTransactionReturnProps extends AxiosResponse {
+  success: boolean
+  message: string
+  data: {
+    reference: string
+    merchant_ref: string
+    payment_selection_type: string
+    payment_method: string
+    payment_name: string
+    customer_name: string
+    customer_email: string
+    customer_phone: string
+    callback_url: string
+    return_url: string
+    amount: number
+    fee_merchant: number
+    fee_customer: number
+    total_fee: number
+    amount_received: number
+    pay_code: string
+    pay_url: string | null
+    checkout_url: string
+    status: string
+    expired_time: number
+    order_items: {
+      sku: string
+      name: string
+      price: number
+      quantity: number
+      subtotal: number
+      product_url: string
+      image_url: string
+    }[]
+    instructions: [
+      {
+        title: string
+        steps: string[]
+      },
+    ]
+    qr_string: string | null
+    qr_url: string | null
+  }
+}
+
 export default function createTripayConfig({
   apiKey,
   privateKey,
   merchant_code,
   isProduction = false,
-}: TripayConfigProps): TripayReturnProps<unknown> {
+}: TripayConfigProps): TripayInputProps<unknown> {
   const endpoint = isProduction
     ? "https://tripay.co.id/api"
     : "https://tripay.co.id/api-sandbox"
@@ -150,7 +341,7 @@ export default function createTripayConfig({
       (allow_html ? `&allow_html=${allow_html}` : "")
 
     try {
-      const { data } = await axios.get(
+      const { data } = await axios.get<InstructionReturnProps>(
         `${endpoint}/payment/instruction?code=${code}${params}`,
         {
           headers: { Authorization: "Bearer " + apiKey },
@@ -158,26 +349,60 @@ export default function createTripayConfig({
       )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+        let errorMessage = "Unknown error occurred"
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
   //Payment Channel
   const paymentChannel = async () => {
     try {
-      const { data } = await axios.get(`${endpoint}/merchant/payment-channel`, {
-        headers: { Authorization: "Bearer " + apiKey },
-      })
+      const { data } = await axios.get<PaymentChannelReturnProps>(
+        `${endpoint}/merchant/payment-channel`,
+        {
+          headers: { Authorization: "Bearer " + apiKey },
+        },
+      )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
   //FEE Calculator
   const feeCalculator = async ({ code, amount }: FeeCalculatorProps) => {
     try {
-      const { data } = await axios.get(
+      const { data } = await axios.get<FeeCalculatorReturnProps>(
         `${endpoint}/merchant/fee-calculator/${
           code ? `?code=${code}&amount=${amount}` : `?amount=${amount}`
         }`,
@@ -187,14 +412,28 @@ export default function createTripayConfig({
       )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+        let errorMessage = "Unknown error occurred"
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
   //Transctions
   const transactions = async ({ page, per_page }: TransctionsProps) => {
     try {
-      const { data } = await axios.get(
+      const { data } = await axios.get<TransactionsReturnProps>(
         `${endpoint}/merchant/transactions?page=${page}&per_page=${per_page} `,
         {
           headers: { Authorization: "Bearer " + apiKey },
@@ -202,14 +441,31 @@ export default function createTripayConfig({
       )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
   //Open Transaction
   const openTransactions = async ({ uuid }: OpenTransactionDetailProps) => {
     try {
-      const { data } = await axios.get(
+      const { data } = await axios.get<OpenTransactionsReturnProps>(
         `https://tripay.co.id/api/open-payment/${uuid}/transactions`,
         {
           headers: { Authorization: "Bearer " + apiKey },
@@ -217,7 +473,24 @@ export default function createTripayConfig({
       )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
@@ -256,7 +529,7 @@ export default function createTripayConfig({
     }
 
     try {
-      const { data } = await axios.post(
+      const { data } = await axios.post<CreateClosedTransactionReturnProps>(
         `${endpoint}/transaction/create`,
         payload,
         {
@@ -265,7 +538,24 @@ export default function createTripayConfig({
       )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
@@ -286,7 +576,7 @@ export default function createTripayConfig({
     }
 
     try {
-      const { data } = await axios.post(
+      const { data } = await axios.post<CreateOpenTransactionReturnProps>(
         "https://tripay.co.id/api/open-payment/create",
         payload,
         {
@@ -296,7 +586,24 @@ export default function createTripayConfig({
 
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
@@ -305,7 +612,7 @@ export default function createTripayConfig({
     reference,
   }: ClosedTransactionDetailProps) => {
     try {
-      const { data } = await axios.get(
+      const { data } = await axios.get<CreateClosedTransactionReturnProps>(
         `${endpoint}/transaction/detail?reference=${reference}`,
         {
           headers: { Authorization: "Bearer " + apiKey },
@@ -314,7 +621,24 @@ export default function createTripayConfig({
 
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
@@ -323,7 +647,7 @@ export default function createTripayConfig({
     uuid,
   }: OpenTransactionDetailProps) => {
     try {
-      const { data } = await axios.get(
+      const { data } = await axios.get<CreateOpenTransactionProps>(
         `https://tripay.co.id/api/open-payment/${uuid}/detail`,
         {
           headers: { Authorization: "Bearer " + apiKey },
@@ -331,7 +655,24 @@ export default function createTripayConfig({
       )
       return data
     } catch (error) {
-      return error
+      const axiosError = error as AxiosError<unknown>
+      if (axiosError.response) {
+        const { status, data } = axiosError.response
+
+        let errorMessage = "Unknown error occurred"
+
+        if (typeof data === "string") {
+          errorMessage = data
+        } else if (data && typeof data === "object" && "message" in data) {
+          errorMessage = data.message as string
+        }
+
+        throw new Error(`Request failed with status ${status}: ${errorMessage}`)
+      } else if (axiosError.request) {
+        throw new Error("No response received from the server")
+      } else {
+        throw new Error("Error setting up the request")
+      }
     }
   }
 
